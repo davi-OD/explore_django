380220f32f05335cf487edc4abc9fe80e656fbec
davi-OD <davidodie51@gmail.com>
2023-08-16 23:53:17 +0300

    working with customr users and more user options ie replacing user management

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
 # Create your models here.
 class Person(models.Model):
@@ -12,3 +13,32 @@ class Person(models.Model):
     # renames the instances of the model
     def __str__(self):
         return self.first_name
+    
+
+# Used to add fields to the user model defaulted
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    address = models.CharField(max_length=30,blank=True)
+    bio_info = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.email
+    
+
+# Used for authentication functionality
+# changing authentication from username to email 
+class Customuser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name= 'email address',
+        max_length=255,
+        unique=True
+    )
+    date_of_birth = models.DateField()
+    is_active= models.BooleanField(default=True)
+    is_admin= models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date of birth']
+
